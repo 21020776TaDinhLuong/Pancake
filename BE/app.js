376741311df2ff144651f3a6a01b4d690e0f07ff
21020776TaDinhLuong <21020776@vnu.edu.vn>
2024-10-29 14:34:00 +0700

    Update app.js

--- BE/app.js
+++ BE/app.js
@@ -28,15 +28,15 @@ app.use('/api/fanpages', fanpageRoutes);
 
 app.use(cors());
 
-
+const NODE_ENV = 'production';
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../frontend/build')));
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './FE/build')));
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    res.sendFile(path.resolve(__dirname, './FE/build/index.html'))
   );
 } else {
   app.get('/', (req, res) => {
