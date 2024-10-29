47d9dc7d07dbd35fc6f8a8cffd876b614ed9c6ec
21020776TaDinhLuong <21020776@vnu.edu.vn>
2024-10-29 15:05:38 +0700

    complete fanpage app

---
+++ app.js
@@ -0,0 +1,56 @@
+import path from 'path';
+import express from 'express';
+import connectDB from './BE/config/db.js';
+import userRoutes from './BE/routes/userRoutes.js';
+import productRoutes from './BE/routes/productRoutes.js';
+import orderRoutes from './BE/routes/orderRoutes.js';
+import careRoutes from './BE/routes/careRoutes.js'
+import postRoutes from './BE/routes/postRoutes.js'
+import fanpageRoutes from './BE/routes/fanpageRoutes.js'
+import cors from 'cors'
+
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+
+app.use('/api/users', userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/cares', careRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/fanpages', fanpageRoutes);
+
+
+
+app.use(cors());
+
+const NODE_ENV = 'production';
+
+const __dirname = path.resolve();
+
+if (NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/FE/build'))); // Đường dẫn chính xác
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'FE', 'build', 'index.html')) // Đường dẫn chính xác đến index.html
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(
+  PORT,
+  console.log(
+    `Server running in development mode on port 5000`
+  )
+);
+
